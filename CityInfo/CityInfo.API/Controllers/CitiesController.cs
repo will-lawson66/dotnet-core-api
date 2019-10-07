dcb0039d9d19dbfce6b88780b642d8f0ed3c9f7f
chikabee <will.lawson@yahoo.com>
2019-10-07 13:35:23 -0400

    add city model and data store representations

--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -12,11 +12,7 @@ namespace CityInfo.API.Controllers
         [HttpGet()]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object>()
-            {
-                new { id = 1, Name = "New York City" },
-                new { id = 2, name = "Antwerp" }
-            });
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
     }
 }
