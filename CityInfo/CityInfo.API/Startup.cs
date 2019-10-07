d2499da53434bf99d2319a45708fbbfe66027065
chikabee <will.lawson@yahoo.com>
2019-10-07 13:01:36 -0400

    add Mvc service and configure

--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -15,6 +15,7 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,13 +30,15 @@ namespace CityInfo.API
                 app.UseExceptionHandler();
             }
 
+            app.UseMvc();
+
 
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("Hello World!");
             //});
 
-            app.Run((context) => throw new Exception("exception"));
+            //app.Run((context) => throw new Exception("exception"));
         }
     }
 }
